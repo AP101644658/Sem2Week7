a6773978eb28c8017be28373517fa453367838ed
Adrian Phillips <101644658@student.swin.edu.au>
2018-09-10 15:12:24 +1000

    Initial/Final

---
+++ BasicAuthDemo/DemoAPI/Models/UserManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Web;
+
+namespace DemoAPI.Models
+{
+    public class UserManager
+    {
+        private DemoDBEntities _Context;
+
+        public UserManager()
+        {
+            _Context = new DemoDBEntities();
+        }
+
+        public bool ValidateUser(string userName, string Password)
+        {
+            var result = _Context.UserMasters.SingleOrDefault(x => x.UserName.Equals(userName, StringComparison.OrdinalIgnoreCase) && x.Password == Password);
+            return result != null ? true : false;
+        }
+    }
+}
